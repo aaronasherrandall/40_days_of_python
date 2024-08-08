498e287ff8afba587158531db835adc011d09569
Aaron Randall <aaronrandall@Aarons-MacBook-Air.local>
2024-08-08 09:30:14 -0400

    day 3

---
+++ bonus/bonus3.py
@@ -0,0 +1,10 @@
+meals = ['pasta', 'pizza', 'salad']
+
+# meal is new variable created on the fly
+# first iteration: meal = "pasta"
+# second iteration: meal = "pizza"
+# third iteration: meal = "salad"
+for meal in 'meals':
+    print(meal.capitalize())
+
+print("Bye!")
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-user_prompt = "Enter a todo:"
-
-# create loop outside of loop
 todos = []
 
 while True:
-    todo = input(user_prompt)
-    print(todo.capitalize())
-    todos.append(todo)
+    # eliminates any trailing spaces
+    user_action = input("Type add, show or exit:").strip()
+    # match case statement is a block
+    match user_action:
+        case "add":
+            todo = input("Add a todo:")
+            todos.append(todo)
+        case "show" | "display":
+            for item in todos:
+                # each first letter in each string is capitalized
+                item = item.title()
+                print(item)
+        case "exit":
+            break
+            print(todos)
+        case _:
+            print("Unknown command entered")
+
+print("Bye!")
